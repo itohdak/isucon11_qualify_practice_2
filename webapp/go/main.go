27abdd87e941e029e10b0eef7ef3cad9aac84c17
itohdak <itohdak@gmail.com>
2024-12-07 14:04:24 +0900

    increase database connection

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -274,7 +274,9 @@ func main() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	db.SetMaxOpenConns(10)
+	db.SetConnMaxLifetime(10 * time.Second)
+	db.SetMaxIdleConns(512)
+	db.SetMaxOpenConns(512)
 	defer db.Close()
 
 	postIsuConditionTargetBaseURL = os.Getenv("POST_ISUCONDITION_TARGET_BASE_URL")
